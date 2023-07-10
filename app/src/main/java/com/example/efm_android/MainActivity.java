308e21a852380464cd4c25421c9350e0ba0f1f3f
Mohamed ESSEBAIY <ahmadessbaiy@gmail.com>
2023-07-10 09:33:51 +0100

    Task02:Create Spinner

--- app/src/main/java/com/example/efm_android/MainActivity.java
+++ app/src/main/java/com/example/efm_android/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.efm_android;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etnumber;
+    Button btncalcul;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etnumber=findViewById(R.id.etnumber);
+        btncalcul=findViewById(R.id.btn_calculate);
+        spinner=findViewById(R.id.spinner);
+
+        @SuppressLint("ResourceType")
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.spinner_items,
+                android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+
     }
 }
